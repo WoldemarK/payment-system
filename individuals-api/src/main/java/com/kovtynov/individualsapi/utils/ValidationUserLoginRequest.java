bca274e47f2c4f85a1f5d17b7d7c255c886c802d
WoldemarK <kovtynov.vladimir@gmail.com>
2025-07-01 00:15:20 +0300

    Настроил сборку DTO

--- individuals-api/src/main/java/com/kovtynov/individualsapi/utils/ValidationUserLoginRequest.java
+++ individuals-api/src/main/java/com/kovtynov/individualsapi/utils/ValidationUserLoginRequest.java
@@ -1,42 +1,41 @@
-//package com.kovtynov.individualsapi.utils;
-//
-//import com.kovtynov.individualsapi.dto.request.UserLoginRequest;
-//import com.kovtynov.individualsapi.dto.request.UserRegistrationRequest;
-//import com.kovtynov.individualsapi.exception.PasswordException;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Component;
-//import reactor.core.publisher.Mono;
-//
-//@Slf4j
-//@Component
-//public class ValidationUserLoginRequest {
-//    public Mono<Void> validationRequestField(UserLoginRequest userLoginRequest) {
-//        log.debug("validationRequestField: {}", userLoginRequest);
-//        return Mono.fromRunnable(() -> {
-//                    if (userLoginRequest == null) {
-//                        throw new IllegalArgumentException("Request is null");
-//                    }
-//                    if (userLoginRequest.password() == null || userLoginRequest.password().isEmpty()) {
-//                        throw new IllegalArgumentException("Password is empty");
-//                    }
-//                    if (userLoginRequest.email() == null || userLoginRequest.email().isEmpty()) {
-//                        throw new PasswordException(
-//                                "Email is empty",
-//                                userLoginRequest.email(),
-//                                "KeycloakUserService",
-//                                HttpStatus.BAD_REQUEST,
-//                                "registerUser"
-//                        );
-//                    }
-//                })
-//                .doOnError(error -> log.error("Validation error: {}", error.getMessage()))
-//                .onErrorResume(error -> {
-//                    if (error instanceof PasswordException) {
-//                        return Mono.error(error);
-//                    } else {
-//                        return Mono.error(new IllegalArgumentException(error.getMessage()));
-//                    }
-//                }).then();
-//    }
-//}
+package com.kovtynov.individualsapi.utils;
+
+import com.kovtynov.individuals.api.dto.UserLoginRequest;
+import com.kovtynov.individualsapi.exception.PasswordException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class ValidationUserLoginRequest {
+    public Mono<Void> validationRequestField(UserLoginRequest userLoginRequest) {
+        log.debug("validationRequestField: {}", userLoginRequest);
+        return Mono.fromRunnable(() -> {
+                    if (userLoginRequest == null) {
+                        throw new IllegalArgumentException("Request is null");
+                    }
+                    if (userLoginRequest.getPassword() == null || userLoginRequest.getPassword().isEmpty()) {
+                        throw new IllegalArgumentException("Password is empty");
+                    }
+                    if (userLoginRequest.getEmail() == null || userLoginRequest.getEmail().isEmpty()) {
+                        throw new PasswordException(
+                                "Email is empty",
+                                userLoginRequest.getEmail(),
+                                "KeycloakUserService",
+                                HttpStatus.BAD_REQUEST,
+                                "registerUser"
+                        );
+                    }
+                })
+                .doOnError(error -> log.error("Validation error: {}", error.getMessage()))
+                .onErrorResume(error -> {
+                    if (error instanceof PasswordException) {
+                        return Mono.error(error);
+                    } else {
+                        return Mono.error(new IllegalArgumentException(error.getMessage()));
+                    }
+                }).then();
+    }
+}
